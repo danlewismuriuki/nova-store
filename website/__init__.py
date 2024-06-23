79a351d2ccfbb6c033cb82843f7f6ca510e6525d
Dan <danlewismuriuki@gmail.com>
2024-06-24 01:53:07 +0300

    feat(database): add method to create database tables
    - Added `create_database` method to create all tables defined by SQLAlchemy models
    - Configured SQLAlchemy with SQLite database URI
    - Included example usage in the Flask application

--- website/__init__.py
+++ website/__init__.py
@@ -4,9 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DB_NAME = 'database.sqlite3'
+
+def create_database():
+    db.create_all()
+    print('Database Created')
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'KJHGWSnbvbno9878ikhgfedf'
+    app.config['SQLACHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
 
     from .views import views
     from .auth import auth
