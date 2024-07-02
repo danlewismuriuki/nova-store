adee4d5814218b1de8579d44bca5bc546aaaa59e
Dan <danlewismuriuki@gmail.com>
2024-07-03 00:56:51 +0300

    fix(admin): Handle product addition errors gracefully in add_shop_items route
    Implemented error handling for database operations in the add_shop_items
    route of the admin module. Added try-except blocks to catch exceptions
    during database addition and commit operations. Flash appropriate error
    messages for user feedback and ensure the form retains user input on
    failure, facilitating correction and resubmission.
    
    Issue #<issue_number>

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template. flash
 from flask_login import login_required, current_user
-
-
+from .forms import ShopItemsForm
+from werkzeug.utils import secure_filename
+from .models import Product
+from . import db
 
 admin = Blueprint('admin', __name__)
 
@@ -10,6 +12,38 @@ admin = Blueprint('admin', __name__)
 def add_shop_items():
     if current_user.id == 1:
         form = ShopItemsForm()
+        if  form.validate_on_submit():
+            product_name = form.product_name.data
+            current_price = form.current_price.data
+            previous_price = form.previous_price.data
+            in_stock = form.in_stock.data
+            flash_sale = form.flash_sale.data
 
-        return rende_template('add-shop-items.html', form=form)
+            file = form.product_picture.data
+            file_name = secure_filename(file.filename)
+            file_path = f'./media/{file_name}'
+
+            file.save(file_path)
+
+            new_shop_item = Product()
+            new_shop_item.product_name = product_name
+            new_shop_item.current_price = current_price
+            new_shop_item.previous_price = previous_price
+            new_shop_item.in_stock = in_stock
+            new_shop_item.flash_sale = flash_sale
+
+            new_shop_item.product_picture = file_path
+
+            try:
+                db.session.add(new_shop_item)
+                db.session.commit()
+                flash(f'{product_name} added Successfully')
+                print('Product added')
+                return render_template('add_shop_items.html', form=form)
+            except Exception as e:
+                print(e)
+                flash('Item Not Added')
+
+        return render_template('add_shop_items.html', form=form)
+
     return render_template('404.html')
