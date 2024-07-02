cad608050a4f95e3162d7f9a5326f4be8871ad2d
Dan <danlewismuriuki@gmail.com>
2024-07-02 17:38:46 +0300

    fix(auth): Resolve NameError for current_user in admin.py
    Add import statement for current_user from flask_login in admin.py to fix NameError when checking user ID.

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 
 admin = Blueprint('admin', __name__)
