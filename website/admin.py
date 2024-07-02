be79bb5cd37b7498cf9f1a2f22b55681611780f5
Dan <danlewismuriuki@gmail.com>
2024-07-03 01:12:53 +0300

    feat: Add functionality to successfully add products
    - Added functionality to allow administrators to add products successfully.
    - Created new HTML template 'add_shop_items.html' for managing product addition.
    - Ensured products can be added and stored correctly in the database.
    
    Changes:
    - Added 'media/' directory to store product images.
    - Renamed 'add-shop-items.html' to 'add_shop_items.html' for consistency.
    
    No issue number

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template. flash
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from .forms import ShopItemsForm
 from werkzeug.utils import secure_filename
