0d75fc5685cd86d0f1e27fd4b522d9ba10f88d54
Dan <danlewismuriuki@gmail.com>
2024-07-06 22:39:35 +0300

    feat(admin): add route for deleting shop items
    - Implemented the delete_item function in admin.py to allow admins to delete items from the shop.
    - Included necessary checks to ensure only the admin (user ID 1) can delete items.
    - Added exception handling to provide feedback if the deletion fails.
    - Updated template with a delete link for each item.
    
    This feature enhances the admin's ability to manage shop items directly from the web interface.

--- website/admin.py
+++ website/admin.py
@@ -107,3 +107,21 @@ def update_item(item_id):
 
         return render_template('update_item.html', form=form)
     return render_template('404.html')
+
+@admin.route('/delete-item/<int:item_id>', methods=['GET', 'POST'])
+@login_required
+def delete_item(item_id):
+    if current_user.id == 1:
+        try:
+            item_to_delete = Product.query.get(item_id)
+            db.session.delete(item_to_delete)
+            db.session.commit()
+            flash('One Item deleted')
+            return redirect('/shop-items')
+
+        except Exception as e:
+            print('Item not deleted', e)
+            flash('Item not deleted!!!')
+        return redirect('/shop-items')
+
+    return render_template('404.html')
